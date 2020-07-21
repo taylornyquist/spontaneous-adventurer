cbd7bf754630e75f9b8ad3070c6909d7c25fd346
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-07-21 16:56:13 -0500

    semi-working modal message

--- assets/js/script.js
+++ assets/js/script.js
@@ -454,33 +454,49 @@ var showPrevious = function (savedLocationsArray) {
     }
 };
 
+//script for error
+function errorMessage() {
+    var message, state;
+    message = document.getElementById("myModal");
+    state = document.getElementById("state-input").value;
+    console.log(state.length);
+    
+    if (state.length === 2) {
+        console.log("everything works!");
+        }
+    else  {
+        message.classList.add("show");
+        message.innerText = "Please use two-digit state abbreviation.";
+            }
+  };
+ 
 // script for Modal
 
-// Get the modal
-var modal = document.getElementById("myModal");
+// // Get the modal
+// var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal CHANGE THIS TO BE TRIGGERED BY ERRORS
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// // When the user clicks the button, open the modal CHANGE THIS TO BE TRIGGERED BY ERRORS
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 // end Modal script
 
 var click = function () {
@@ -488,6 +504,7 @@ var click = function () {
     var city = document.querySelector(".search-city").value;
     var state = $("#state-input").val().trim().toLowerCase();
 
+    errorMessage(state);
     saveLocation(city, state);
     getNPSData(state);
     getTickemaster(city);
