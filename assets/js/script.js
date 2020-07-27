aa2c68c3f2a72832a0c562b5cff3b6676c940498
Taylor Nyquist <tnyquist@gmail.com>
2020-07-26 19:57:49 -0500

    updated page title and removed console logs and commented out code

--- assets/js/script.js
+++ assets/js/script.js
@@ -209,7 +209,7 @@ function getTickemaster(city) {
                 }
             );
 
-            console.log(ticketmasterResponse);
+            // console.log(ticketmasterResponse);
             
             //For loop to get data from ticketmaster API
             for (i = 0; i < 5; i++) {
@@ -483,35 +483,6 @@ function errorMessage() {
         searchClick();
     }
 };
-
-// script for Modal
-
-// // Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks the button, open the modal CHANGE THIS TO BE TRIGGERED BY ERRORS
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-// end Modal script
 
 var checkError = function () {
     errorMessage();
